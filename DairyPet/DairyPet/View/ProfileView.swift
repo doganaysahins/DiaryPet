764f27b102eba251c770f3e8271bd3eee7a8f10c
Doganay <doganayssahin@gmail.com>
2022-06-11 19:14:43 +0300

    changes on profile page

--- DairyPet/DairyPet/View/ProfileView.swift
+++ DairyPet/DairyPet/View/ProfileView.swift
@@ -44,8 +44,6 @@ struct ProfileView: View {
                             }
                             Spacer()
                         }
-                       
-
                     }
             }
         
